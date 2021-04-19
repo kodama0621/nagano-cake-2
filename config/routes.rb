49dd9885b8927da6f00a5c198fbbc81b470474cc
kodama0621 <eryuhikari@gmail.com>
2021-04-19 11:34:04 +0000

    [Update]view編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
 Rails.application.routes.draw do
+
   get 'admin' => 'admin/homes#top'
   root :to => 'public/homes#top'
   get 'about' => 'public/homes#about'
+
+  devise_for :admins, controllers: {
+    sessions:      'admin_devises/sessions',
+    passwords:     'admin_devises/passwords',
+    registrations: 'admin_devises/registrations'
+  }
   namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
   end
+
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
 
   namespace :public do
     resources :items, only: [:index, :show]
